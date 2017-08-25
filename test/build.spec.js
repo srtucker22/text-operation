a90040bd1aed86eff1d8ba0cea21a208be6c11dd
Simon Tucker <srtucker22@gmail.com>
2017-08-25 13:22:52 -0700

    with tests

--- test/build.spec.js
+++ test/build.spec.js
@@ -1,3 +1,15 @@
-const myModule = require('../dist/index');
+const modules = require('../dist/index');
 
-console.log(myModule);
+describe('Build Tests', () => {
+  it('Creates a module', () => {
+    function cursorTest() {
+      return modules.Cursor.fromJSON({ position: 3, selectionEnd: 5 });
+    }
+    expect(cursorTest).not.toThrow();
+
+    function textOperationTest() {
+      return modules.TextOperation.fromJSON([2, -1, -1, 'cde']);
+    }
+    expect(textOperationTest).not.toThrow();
+  });
+});
